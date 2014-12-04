7acec0f7dbd9b3a96c3603cefa64dc157ebf8b9d
rparkchan <rparkchan@college.harvard.edu>
2014-12-04 00:22:24 -0500

    GOOOD

--- WikiTag/ViewControllerThree.swift
+++ WikiTag/ViewControllerThree.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ViewControllerThree.swift
 //  WikiTag
 //
 //  Created by James Hotchkiss on 11/26/14.
@@ -8,23 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewControllerThree: UIViewController {
     
-
+    
+    @IBOutlet var currentWebView: UIWebView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        var randomURL = "http://en.wikipedia.org/wiki/Special:Random"
+        var url = NSURL(string: randomURL)
+        var currentRequest = NSURLRequest(URL: url!)
+        currentWebView.loadRequest(currentRequest)
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    
-    
-    
-
 }
-
