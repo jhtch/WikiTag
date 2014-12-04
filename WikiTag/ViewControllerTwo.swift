ced84dba2b95d239aa4709b5fd6a351aba2046f5
rparkchan <rparkchan@college.harvard.edu>
2014-12-03 23:58:38 -0500

    GOOD VERSION

--- WikiTag/ViewControllerTwo.swift
+++ WikiTag/ViewControllerTwo.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ViewControllerTwo.swift
 //  WikiTag
 //
 //  Created by James Hotchkiss on 11/26/14.
@@ -8,27 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewControllerTwo: UIViewController {
     
     
-   
-    @IBOutlet var textLabel:UILabel!
-
+    @IBOutlet var destinationWebView: UIWebView!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        var randomURL = "http://en.wikipedia.org/wiki/Special:Random"
+        var url = NSURL(string: randomURL)
+        var destinationRequest = NSURLRequest(URL: url!)
+        destinationWebView.loadRequest(destinationRequest)
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    
-    
-    
-
 }
 
