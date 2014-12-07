4c0c2f8c7bb20262f01b1cdba2fac263c14cc201
rparkchan <rparkchan@college.harvard.edu>
2014-12-07 01:55:07 -0500

    yay

--- WikiTag/ViewControllerFour.swift
+++ WikiTag/ViewControllerFour.swift
@@ -13,6 +13,9 @@ class ViewControllerFour: UIViewController {
     @IBOutlet var counterLabel: UILabel!
     var finalCounter:Double!
     
+    @IBOutlet var initials: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,12 +24,24 @@ class ViewControllerFour: UIViewController {
         var counterString:String = String(format: "%d", counterInt)
         counterLabel.text = counterString
         
+        var gameScore = PFObject(className: "GameScore")
+        
     }
     
+    @IBAction func submitScore(sender: AnyObject) {
+        var initialsText = initials.text
+        var object = PFObject(className: "RecentScores")
+        object.addObject("Jump1", forKey: "startPage")
+        object.addObject("Jump2", forKey: "endPage")
+        object.addObject(Int(finalCounter) + 1, forKey: "jumps")
+        object.addObject(initialsText, forKey: "initials")
+        object.saveInBackground()
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+   
 }
 
